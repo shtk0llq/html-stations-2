b9d75c52d8d1b0dc0dca0065257147ff7ca70169
shtk0llq <shtk0llq@gmail.com>
2024-08-21 09:43:56 +0900

    station 13

--- src/scripts/station13.js
+++ src/scripts/station13.js
@@ -1,4 +1,10 @@
 function changeBackgroundColor(e) {
   // ここに背景色を変える処理を書く。
+  const textElement = document.getElementById('text');
 
+  if (e.target.checked) {
+    textElement.style.backgroundColor = 'red';
+  } else {
+    textElement.style.backgroundColor = '';
+  }
 }
